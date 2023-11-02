84dffce11b10d04ed3a68494038e8cf41179202f
“Spencer <spencer@bookservo.com>
2023-11-02 15:40:39 -0400

    types api

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    get 'types', to: 'pokemon#types', as: "pokemon_types"
+    resources :items, only: [:index, :create, :update, :destroy]
+    resources :pokemon, only: [:index, :create, :show, :update]
+  end
 end
